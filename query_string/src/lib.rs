448533a5dad93d272dff8e14bdcf8cfabfcc4a52
Jacob Birkett <jacob@birkett.dev>
2022-04-08 00:52:29 -0700

    Reformat imports in query_string proc-macro

--- query_string/src/lib.rs
+++ query_string/src/lib.rs
@@ -1,8 +1,7 @@
 pub mod __uses {
     pub use form_urlencoded::Serializer as QuerySerializer;
     pub use serde::Serialize as SerdeSerialize;
-    pub use serde_json::to_value as serde_json_to_value;
-    pub use serde_json::Value as JsonValue;
+    pub use serde_json::{to_value as serde_json_to_value, Value as JsonValue};
 }
 
 pub use query_string_derive::QueryString;
